1614df42f723e1523ee4ee69e35693d82f4859e5
harshapat <harsha.patankar@gmail.com>
2017-10-29 21:47:13 +0000

    initial commit of test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import MembersContainer from './containers/MembersContainer';
+import MessagesContainer from './containers/MessagesContainer';
+import { Messenger } from './components/Messenger';
+
+
 class App extends Component {
   render() {
+    // return (
+    //   <h1>
+    //     Hello Robin!
+    //   </h1>
+    // );
     return (
-      <h1>
-        Hello Robin!
-      </h1>
+      <MembersContainer>
+        <MessagesContainer>
+          <Messenger />
+        </MessagesContainer>
+      </MembersContainer>
     );
   }
 }
--- src/service.js
+++ src/service.js
@@ -1,11 +1,29 @@
+import { getMessages, getMembers } from './data';
+
+
+// export default function getChatLog() {
+//   return Promise.resolve([{
+//     messageId: '12356',
+//     userId: '613651251',
+//     fullName: 'Robin Balmforth',
+//     timestamp: new Date().toISOString(),
+//     email: 'robin@example.com',
+//     message: 'Hello, World!',
+//     avatar: null
+//   }]);
+// };
+
+
+export function getMembersList() {
+  return {
+    type: 'MEMBERS_LOADING',
+    payload: getMembers()
+  }
+}
+
 export default function getChatLog() {
-  return Promise.resolve([{
-    messageId: '12356',
-    userId: '613651251',
-    fullName: 'Robin Balmforth',
-    timestamp: new Date().toISOString(),
-    email: 'robin@example.com',
-    message: 'Hello, World!',
-    avatar: null
-  }]);
-};
+  return {
+    type: 'MESSAGES_LOADING',
+    payload: getMessages()
+  };
+}
